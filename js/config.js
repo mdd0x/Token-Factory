d2248de2ecde9988ec38abdf3f45f40ca00dd657
Mdd 0x <adityamahaputra18@gmail.com>
2022-12-22 04:42:27 +0700

    a

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 // 3.client total fee= contract small fee + your setting fee
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var FeeReciever = "0xEAc8555E83919Cedc065849b0FDc843bC03EFd4d";
+var FeeReciever = "0x8B1e4796493dDEf4d5Ba34fBEa481b5943338F30";
 
 
 var BinanceFee=0.01;
